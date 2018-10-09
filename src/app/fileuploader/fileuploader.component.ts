0e549ec38dd2522a6f324296389948c83b8bfaf2
Cristian Predan <cristianpredan@Cristians-MacBook-Pro.local>
2018-10-09 19:01:23 +0300

    Worked on the FileUploader component

--- src/app/fileuploader/fileuploader.component.ts
+++ src/app/fileuploader/fileuploader.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-uploader',
   templateUrl: './fileuploader.component.html',
   styleUrls: ['./fileuploader.component.css']
 })
-export class FileuploaderComponent implements OnInit {
+export class FileuploaderComponent {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  uploadFile(event) {
+    let elem = event.target;
+    if(elem.files.length > 0){
+      console.log(elem.files[0]);
+      let formData = new FormData();
+      formData.append('file', elem.files[0]);
+    }
   }
 
 }
